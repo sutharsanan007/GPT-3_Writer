a66759561da792cdcbb6ac0768239ac9ceffc6eb
sutharsanan007 <sutharsananravi@gmail.com>
2023-07-18 20:52:40 -0400

    Final Commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+  
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
   return (
     <div className="root">
       <Head>
@@ -11,11 +42,38 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Ask Kate to land on your dream job</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Unlock the Key to Your Perfect Job with Kate - Let Her Know What dream job you want to land on and She'll Help You Unlock Any Opportunity, No Matter the Sector!</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea 
+            placeholder="Enter the name of the job you are searching for!" 
+            className="prompt-box" 
+            value={userInput} 
+            onChange={onUserChangedText} 
+          />
+          <div className="prompt-buttons">
+            <a className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={callGenerateEndpoint}>
+              <div className="generate">
+              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
         </div>
       </div>
       <div className="badge-container grow">
